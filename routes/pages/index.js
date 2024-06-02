b604e8a2be177c05fbf244e9ca082a8154b69f95
Davide <davide.gioia.private@gmail.com>
2024-06-03 00:57:22 +0200

    Sezione Changelog sincronizzata con github aggiunta

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -6,16 +6,30 @@ const generateTxt = require('../generateTxt.js')
 const updateStock = require('../update/updateStock.js');
 const updateDB = require('../update/updateDB'); 
 const managePrinters = require('../managePrinters.js');
+const simpleGit = require('simple-git');
+
 var router = express.Router();
 
+const git = simpleGit();
 let allPrinters = printerList();
+
 updateDB();
 setInterval(updateDB, settings.interval);
 
 router.get('/', async function(req, res, next) {
     let allPrinters = await printerList();
     let textPrinters = await generateTxt(allPrinters);
-    res.render('index', {printer: allPrinters, txt: textPrinters});    
+
+    const log = await git.log();
+    const commit = log.all.map(commit => ({            
+        hash: commit.hash,
+        date: commit.date,
+        message: commit.message,
+        author: commit.author_name
+    }));
+    console.log("ciao" + git);
+
+    res.render('index', {printer: allPrinters, txt: textPrinters, commits: commit});    
 })
 
 router.post('/', async function(req, res, next) {
